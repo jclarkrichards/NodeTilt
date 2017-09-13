6f9140b13e211190671c5fe2475ade63bdac7778
Jonathan <jclarkrichards@gmail.com>
2017-09-12 21:49:45 -0400

    Commiting more

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Node
+{
+    public Vector3 position = new Vector3();
+    public Dictionary<direction, Node> neighbors = new Dictionary<direction, Node>();
+
+    public Node(Vector3 p)
+    {
+        position = p;       
+    }
+
+    public void AddNeighbor(Node node, direction D)
+    {
+        neighbors.Add(D, node);
+    }
+    
+}
